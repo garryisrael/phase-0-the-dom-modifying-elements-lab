4c97fce3f8a0f00fac82c201825d21d753b3bc83
Garry <garryisrael02@gmail.com>
2023-05-25 02:04:57 -0400

    Modifies elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const name = "Garry Israel"
+document.getElementById("main").remove()
+let newHeader = document.createElement ("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = `${name} is the champion`
+body.append(newHeader)
+newHeader.textContent
+
+
+
